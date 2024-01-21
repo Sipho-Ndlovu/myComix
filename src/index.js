0fbfe9639dec9ee778f6592a6d8c566905b5f104
Sipho-ndlovu <siphosenkosii.ndlovu@gmail.com>
2024-01-21 18:53:12 +0000

    Implementd code for login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const login = document.querySelector('.btnLogin');
 const signup = document.querySelector('.btnSignup');
 
-
 $('#loginForm').submit(function (e) {
     e.preventDefault();
     var username = $('#loginUsername').val();
@@ -28,6 +27,38 @@ $('#loginForm').submit(function (e) {
     });
 });
 
+$('#signupForm').submit(function (e) {
+    e.preventDefault();
+    var name = $('#signupName').val();
+    var email = $('#signupEmail').val();
+    var username = $('#signupUsername').val();
+    var password = $('#signupPassword').val();
+    var confirmPassword = $('#signupConfirmPassword').val();
+    var submit = $('#signupSubmit').val();
+    $.post('src/auth.php', {
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+        confirmPassword: confirmPassword,
+        submit: submit
+    }, function (data) {
+        $('#signupMessageContainer').html(data);
+
+        if (data.includes('error')) {
+            $('#signupMessageContainer').addClass('error');
+        } else {
+            $('#signupMessageContainer').removeClass('error');
+        }
+
+        if (data.includes('success')) {
+            $('#signupMessageContainer').addClass('success');
+        } else {
+            $('#signupMessageContainer').removeClass('success');
+        }
+    });
+});
+
 login.onclick = () => {
     const signupForm = document.querySelector('.signupForm');
     const loginForm = document.querySelector('.loginForm');
@@ -43,37 +74,5 @@ signup.onclick = () => {
     if (login) {
         signupForm.style.display = 'flex';
         loginForm.style.display = 'none';
-
-        $('#signupForm').submit(function (e) {
-            e.preventDefault();
-            var name = $('#signupName').val();
-            var email = $('#signupEmail').val();
-            var username = $('#signupUsername').val();
-            var password = $('#signupPassword').val();
-            var confirmPassword = $('#signupConfirmPassword').val();
-            var submit = $('#signupSubmit').val();
-            $.post('src/auth.php', {
-                name: name,
-                email: email,
-                username: username,
-                password: password,
-                confirmPassword: confirmPassword,
-                submit: submit
-            }, function (data) {
-                $('#signupMessageContainer').html(data);
-
-                if (data.includes('error')) {
-                    $('#signupMessageContainer').addClass('error');
-                } else {
-                    $('#signupMessageContainer').removeClass('error');
-                }
-
-                if (data.includes('success')) {
-                    $('#signupMessageContainer').addClass('success');
-                } else {
-                    $('#signupMessageContainer').removeClass('success');
-                }
-            });
-        });
     }
 }
